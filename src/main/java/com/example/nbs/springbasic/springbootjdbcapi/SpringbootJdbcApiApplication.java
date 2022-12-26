eaa5d42f8f1bf27c3f25996e8b93da2746d2464c
tunguyen <tunguyenkawa@gmail.com>
2022-12-27 06:48:03 +0700

    Generated from Spring Initializer: Spring Web, JDBC API, H2DB

---
+++ src/main/java/com/example/nbs/springbasic/springbootjdbcapi/SpringbootJdbcApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbs.springbasic.springbootjdbcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nbs/springbasic/springbootjdbcapi/SpringbootJdbcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbs.springbasic.springbootjdbcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
